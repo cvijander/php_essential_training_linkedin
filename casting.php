a47f73d71da0629f25b261f67f7d372df7aabcda
cvijander <cvija85@gmail.com>
2023-07-30 22:25:50 +0200

    casting

--- casting.php
+++ casting.php
@@ -1,5 +1,35 @@
 <?php 
 
+$count ="2 cats ";
+echo "Value count is: " . gettype($count)."<br/>";
+
+$count +=3;
+echo "After adding number 3 to string 2 now it is : <br/>";
+echo "Value count is: " . gettype($count)."<br/>";
+
+echo $cats = "I have " .$count . " cats. "."<br/>";
+echo "Cats : " .gettype($cats). "<br/>";
+
+echo "<br/>";
+
+echo "Type casting :<br/>";
+settype($count,"integer");
+echo "Count variable : " .gettype($count). "<br/>";
+
+$count2 = (string) $count;
+echo "Count: " .gettype($count) . "<br/>";
+echo "Count2 :" .gettype($count2) . "<br/>";
+
+
+$test1 =3; 
+$test2 = 3;
+settype($test1,"string");
+(string)$test2; 
+
+echo "Test1 " .gettype($test1)."<br/>";
+echo "Test2 " . gettype($test2). "<br/>";
+
+
 
 
 
