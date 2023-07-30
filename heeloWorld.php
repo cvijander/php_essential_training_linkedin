99e86871df422fe06b4a8e684fe8d7d1290de91e
cvijander <cvija85@gmail.com>
2023-07-30 12:56:54 +0200

    hello

---
+++ heeloWorld.php
@@ -0,0 +1,18 @@
+<?php 
+
+// single line comments are like this
+# or like this less common
+echo"Hello World!"; 
+
+echo "Hello " . " world!";
+echo 2+3;
+
+/* double line comments are written
+ like this , so you can keep typing
+ and typing 
+ */
+
+ 
+
+
+?>
