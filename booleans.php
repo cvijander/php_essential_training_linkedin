485bd4889495d207fe6a03d3f4dec52082efc8ca
cvijander <cvija85@gmail.com>
2023-07-30 20:15:19 +0200

    booleans

--- booleans.php
+++ booleans.php
@@ -6,7 +6,12 @@ $result2 = false;
 echo $result1 . "<br/>";
 echo $result2 . "<br/>";
 
-echo "Result is boolean " . is_bool($result2). "<br/>";
+echo "Result is boolean ?" . is_bool($result2). "<br/>";
+
+$number =3.14;
+if(is_float($number)){
+    echo "It is a float.";
+}
 
 
 
--- null.php
+++ null.php
@@ -1,5 +1,15 @@
 <?php 
 
+$var1 = null;
+$var2 = "";
+
+echo "Var1 null ? ". is_null($var1). "<br/>";
+echo "Var 2 null? " .is_null($var2). "<br/>";
+echo "Var3 null ? " .is_null($var3) . "<br/>";
+
+echo "Var1 is set " . isset($var1). "<br/>";
+echo "Var2 is set " . isset($var2). "<br/>";
+echo "Var3 is set " .isset($var3) . "<br/>";
 
 
 
